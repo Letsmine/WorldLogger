efb3cab5fbc9b6c9526214a22ca487995cf097fe
KleinCrafter <GitHub@clan-300.de>
2018-08-19 09:06:11 +0200

    First Commit

---
+++ src/main/java/eu/letsmine/worldlogger/WorldLoggerPlugin.java
@@ -0,0 +1,9 @@
+package eu.letsmine.worldlogger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WorldLoggerPlugin extends JavaPlugin {
+	
+	
+	
+}
---
+++ src/main/java/eu/letsmine/worldlogger/listener/BlockListener.java
@@ -0,0 +1,12 @@
+package eu.letsmine.worldlogger.listener;
+
+import eu.letsmine.bukkit.listener.IBukkitListener;
+import eu.letsmine.worldlogger.WorldLoggerPlugin;
+
+public class BlockListener extends IBukkitListener<WorldLoggerPlugin> {
+
+	public BlockListener(WorldLoggerPlugin plugin) {
+		super(plugin);
+	}
+	
+}
